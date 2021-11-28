3d4fe4923b8f308caa1953aab1dbab9b1150fc08
AloofSkip <nic.veloce@gmail.com>
2021-11-27 22:45:50 -0800

    Prepare new worksheet and set up getJSON function

--- pages/api/courses/[courseID].ts
+++ pages/api/courses/[courseID].ts
@@ -1,14 +1,13 @@
 import type { NextApiRequest, NextApiResponse } from "next";
 import connectDB from "../../../util/mongodb";
 import CourseScema from "../../../util/models/course";
-import getDocuments from "../../../util/sheets";
+import {} from "../../../util/sheets";
 
 type Data = {
     name: string;
 };
 
 function handler(req: NextApiRequest, res: NextApiResponse<Data>) {
-    getDocuments();
     res.status(200).json({ name: "John Doe" });
 }
 
--- util/sheets.ts
+++ util/sheets.ts
@@ -1,24 +1,75 @@
 const { GoogleSpreadsheet } = require("google-spreadsheet");
 
+const { WorksheetType } = require("@types/google-spreadsheet");
+
 const { GOOGLE_CLIENT_EMAIL, GOOGLE_PRIVATE_KEY } = process.env;
 
-const originalSpreadsheet = new GoogleSpreadsheet(
-    "1MFBGJbOXVjtThgj5b6K0rv9xdsC1M2GQ0pJVB-8YCeU"
-);
-//const newSpreadsheet = new GoogleSpreadsheet("");
+const originalSpreadsheetID = "1MFBGJbOXVjtThgj5b6K0rv9xdsC1M2GQ0pJVB-8YCeU";
+const originalSpreadsheet = new GoogleSpreadsheet(originalSpreadsheetID);
+const newSpreadsheetID = "1NEIcb6ab5HTeR_fEurwRIRbnXjb8upCb_yB0FVR7X58";
+const newSpreadsheet = new GoogleSpreadsheet(newSpreadsheetID);
+const authCredentials = {
+    client_email: GOOGLE_CLIENT_EMAIL,
+    private_key: GOOGLE_PRIVATE_KEY?.replace(/\\n/g, "\n"),
+};
 
-async function getDocuments() {
+async function getJSON(worksheet: typeof WorksheetType) {
     try {
-        await originalSpreadsheet.useServiceAccountAuth({
-            client_email: GOOGLE_CLIENT_EMAIL,
-            private_key: GOOGLE_PRIVATE_KEY?.replace(/\\n/g, "\n"),
-        });
+        const rows = await worksheet.getRows();
+        const rowsCount = rows.length;
 
-        await originalSpreadsheet.loadInfo(); // loads document properties and worksheets
-        console.log(originalSpreadsheet.title);
+        for (let row = 0; row < rowsCount; row++) {}
     } catch (e) {
         console.error(e);
     }
 }
 
-export default getDocuments;
+async function prepareSheet() {
+    try {
+        await newSpreadsheet.useServiceAccountAuth(authCredentials);
+        await originalSpreadsheet.useServiceAccountAuth(authCredentials);
+        await newSpreadsheet.loadInfo();
+        await originalSpreadsheet.loadInfo();
+
+        /* Delete old spreadsheet */
+        const oldSheet = newSpreadsheet.sheetsByIndex[1];
+        if (oldSheet) {
+            oldSheet.delete();
+        }
+
+        /* Copy original data to newSpreadsheet */
+        originalSpreadsheet.sheetsByIndex[0]
+            .copyToSpreadsheet(newSpreadsheetID)
+            .then(async () => {
+                /* Reload newSpreadsheet data to prevent errors */
+                await newSpreadsheet.loadInfo();
+                let newSheet = newSpreadsheet.sheetsByIndex[1];
+
+                /* Update newSheet headers to remove duplicates from original */
+                newSheet.setHeaderRow([
+                    "timestamp",
+                    "course1",
+                    "difficulty1",
+                    "time1",
+                    "tips1",
+                    "when1",
+                    "secondBool",
+                    "course2",
+                    "difficulty2",
+                    "time2",
+                    "tips2",
+                    "thirdBool",
+                    "course3",
+                    "difficulty3",
+                    "time3",
+                    "tips3",
+                ]);
+
+                return newSheet;
+            });
+    } catch (e) {
+        console.error(e);
+    }
+}
+
+export {};
