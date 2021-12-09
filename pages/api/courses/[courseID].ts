5a36ef69ec25e4a028e9abadd8b296ff9891e9dc
AloofSkip <nic.veloce@gmail.com>
2021-12-08 18:05:23 -0800

    Adds a temp fix for regex query

--- pages/api/courses/[courseID].ts
+++ pages/api/courses/[courseID].ts
@@ -1,14 +1,16 @@
 import type { NextApiRequest, NextApiResponse } from "next";
 import { connectToDatabase } from "../../../util/mongodb";
-import { Course } from "../../../util/models/course";
+import { Course, ICourse } from "../../../util/models/course";
 
 async function getCourses(req: NextApiRequest, res: NextApiResponse) {
     if (req.method === "GET") {
         try {
             await connectToDatabase();
             const { courseID } = req.query;
-            const courses = await Course.find({
-                name: { $regex: courseID, $options: "i" },
+
+            const courses: ICourse[] = await Course.find({
+                /* Hacky Type Fix */
+                name: { $regex: courseID, $options: "i" } as unknown as string,
             });
             res.status(200).json(courses);
         } catch (err) {
