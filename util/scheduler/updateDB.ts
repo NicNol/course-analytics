5eaac529fc329d908b77e1fdb0b9509ddc5b4aa4
AloofSkip <nic.veloce@gmail.com>
2021-12-20 10:57:03 -0800

    Update package.json scripts

--- util/scheduler/updateDB.ts
+++ util/scheduler/updateDB.ts
@@ -4,7 +4,6 @@ import { summarizeData, emptyDB, saveCourses } from "./helper/summarize";
 export default async function updateDB() {
     await prepareSheet();
     const json = await getJSON();
-    await console.log(json[0]);
     await emptyDB();
     await saveCourses(json);
     await summarizeData();
