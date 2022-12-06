fcb5641546a4279c90082b6b072324a34c3257ee
AloofSkip <nic.veloce@gmail.com>
2022-12-05 19:00:39 -0800

    add vercel analytics to root of react project

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,13 +1,17 @@
 import "../styles/globals.css";
 import type { AppProps } from "next/app";
 import { ChakraProvider } from "@chakra-ui/react";
+import { Analytics } from "@vercel/analytics/react";
 
 function MyApp({ Component, pageProps }: AppProps) {
-    return (
-        <ChakraProvider>
-            <Component {...pageProps} />
-        </ChakraProvider>
-    );
+  return (
+    <>
+      <ChakraProvider>
+        <Component {...pageProps} />
+      </ChakraProvider>
+      <Analytics />
+    </>
+  );
 }
 
 export default MyApp;
