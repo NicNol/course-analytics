c1804a6e4d30c78dd1c9648dbaa94794879ba625
Nic Nolan <nic.veloce@gmail.com>
2022-12-19 15:40:45 -0800

    improve scraping scripts (#51)

--- util/scheduler/helper/sheets.ts
+++ util/scheduler/helper/sheets.ts
@@ -1,13 +1,12 @@
-const { GoogleSpreadsheet } = require("google-spreadsheet");
-import { GoogleSpreadsheetWorksheet } from "google-spreadsheet";
+import { GoogleSpreadsheet, GoogleSpreadsheetWorksheet } from "google-spreadsheet";
 import { ICourse } from "../../models/course";
 import { formatCourseName } from "./summarize";
 
 /* Get credentials from process.env */
 const { GOOGLE_CLIENT_EMAIL, GOOGLE_PRIVATE_KEY } = process.env;
 const authCredentials = {
-    client_email: GOOGLE_CLIENT_EMAIL,
-    private_key: GOOGLE_PRIVATE_KEY?.replace(/\\n/g, "\n"),
+  client_email: GOOGLE_CLIENT_EMAIL || "GOOGLE_CLIENT_EMAIL is missing",
+  private_key: GOOGLE_PRIVATE_KEY?.replace(/\\n/g, "\n") || "GOOGLE_PRIVATE_KEY is missing",
 };
 
 /* Declare and initialize spreadsheet objects */
@@ -17,110 +16,104 @@ const newSpreadsheetID = "1NEIcb6ab5HTeR_fEurwRIRbnXjb8upCb_yB0FVR7X58";
 const newSpreadsheet = new GoogleSpreadsheet(newSpreadsheetID);
 
 async function prepareSheet(): Promise<any> {
-    try {
-        /* Load our duplicate Workbook */
-        await newSpreadsheet.useServiceAccountAuth(authCredentials);
-        await newSpreadsheet.loadInfo();
+  try {
+    /* Load our duplicate Workbook */
+    await newSpreadsheet.useServiceAccountAuth(authCredentials);
+    await newSpreadsheet.loadInfo();
 
-        /* Delete the last spreadsheet */
-        const oldSheet = newSpreadsheet.sheetsByIndex[1];
-        if (oldSheet) {
-            oldSheet.delete();
-        }
+    /* Delete all but the first worksheet */
+    newSpreadsheet.sheetsByIndex.forEach(async (sheet, index) => {
+      if (index > 0) await sheet.delete();
+    });
 
-        /* Load the course reviews source Workbook */
-        await originalSpreadsheet.useServiceAccountAuth(authCredentials);
-        await originalSpreadsheet.loadInfo();
+    /* Load the course reviews source Workbook */
+    await originalSpreadsheet.useServiceAccountAuth(authCredentials);
+    await originalSpreadsheet.loadInfo();
 
-        /* Copy original data to newSpreadsheet */
-        await originalSpreadsheet.sheetsByIndex[0].copyToSpreadsheet(
-            newSpreadsheetID
-        );
+    /* Copy original data to newSpreadsheet */
+    await originalSpreadsheet.sheetsByIndex[0].copyToSpreadsheet(newSpreadsheetID);
 
-        /* Reload newSpreadsheet data to prevent errors */
-        await newSpreadsheet.loadInfo();
-        let newSheet: GoogleSpreadsheetWorksheet = await newSpreadsheet
-            .sheetsByIndex[1];
+    /* Reload newSpreadsheet data to prevent errors */
+    await newSpreadsheet.loadInfo();
+    const newSheet: GoogleSpreadsheetWorksheet = newSpreadsheet.sheetsByIndex[1];
 
-        /* Update newSheet headers to remove duplicates from original */
-        await newSheet.setHeaderRow([
-            "timestamp",
-            "course1",
-            "difficulty1",
-            "time1",
-            "tips1",
-            "when1",
-            "secondBool",
-            "course2",
-            "difficulty2",
-            "time2",
-            "tips2",
-            "thirdBool",
-            "course3",
-            "difficulty3",
-            "time3",
-            "tips3",
-        ]);
-    } catch (err) {
-        throw err;
-    }
+    /* Update newSheet headers to remove duplicates from original */
+    await newSheet.setHeaderRow([
+      "timestamp",
+      "course1",
+      "difficulty1",
+      "time1",
+      "tips1",
+      "when1",
+      "secondBool",
+      "course2",
+      "difficulty2",
+      "time2",
+      "tips2",
+      "thirdBool",
+      "course3",
+      "difficulty3",
+      "time3",
+      "tips3",
+    ]);
+  } catch (err) {
+    throw err;
+  }
 }
 
 async function getJSON(): Promise<ICourse[]> {
-    try {
-        /* Load our duplicate spreadsheet */
-        await newSpreadsheet.useServiceAccountAuth(authCredentials);
-        await newSpreadsheet.loadInfo();
+  try {
+    /* Load our duplicate spreadsheet */
+    await newSpreadsheet.useServiceAccountAuth(authCredentials);
+    await newSpreadsheet.loadInfo();
 
-        /* Declare and initialize variables */
-        const worksheet = newSpreadsheet.sheetsByIndex[1];
-        const rows = await worksheet.getRows();
-        const output = [];
+    /* Declare and initialize variables */
+    const worksheet = newSpreadsheet.sheetsByIndex[1];
+    const rows = await worksheet.getRows();
+    const output = [];
 
-        /* Create objects from each row of the spreadsheet */
-        for (const row of rows) {
-            /* All rows have at least one course review */
-            let courses = 1;
-            const firstCourse = formatCourseName(row.course1);
-            const courseNames = [firstCourse];
+    /* Create objects from each row of the spreadsheet */
+    for (const row of rows) {
+      /* All rows have at least one course review */
+      let courses = 1;
+      const firstCourse = formatCourseName(row.course1);
+      const courseNames = [firstCourse];
 
-            /* Check for second course review */
-            if (row.secondBool === "Yes") {
-                const secondCourse = formatCourseName(row.course2);
-                courseNames.push(secondCourse);
-                courses++;
-            }
+      /* Check for second course review */
+      if (row.secondBool === "Yes") {
+        const secondCourse = formatCourseName(row.course2);
+        courseNames.push(secondCourse);
+        courses += 1;
+      }
 
-            /* Check for third course review */
-            if (row.thirdBool === "Yes") {
-                const thirdCourse = formatCourseName(row.course3);
-                courseNames.push(thirdCourse);
-                courses++;
-            }
+      /* Check for third course review */
+      if (row.thirdBool === "Yes") {
+        const thirdCourse = formatCourseName(row.course3);
+        courseNames.push(thirdCourse);
+        courses += 1;
+      }
 
-            /* Create an ICourse object for each review in the spreadsheet row*/
-            for (let i = 1; i <= courses; i++) {
-                const formattedName = formatCourseName(row[`course${i}`]);
-                const course: ICourse = {
-                    name: formattedName,
-                    difficulty: row[`difficulty${i}`],
-                    "time commitment": row[`time${i}`],
-                    review: row[`tips${i}`],
-                    "review date": row.timestamp,
-                    quarter: row.when1,
-                    "other courses": courseNames.filter(
-                        (courseName) => courseName !== formattedName
-                    ),
-                };
+      /* Create an ICourse object for each review in the spreadsheet row*/
+      for (let i = 1; i <= courses; i++) {
+        const formattedName = formatCourseName(row[`course${i}`]);
+        const course: ICourse = {
+          name: formattedName,
+          difficulty: row[`difficulty${i}`],
+          "time commitment": row[`time${i}`],
+          review: row[`tips${i}`],
+          "review date": row.timestamp,
+          quarter: row.when1,
+          "other courses": courseNames.filter((courseName) => courseName !== formattedName),
+        };
 
-                /* Push the ICourse object to the output array */
-                output.push(course);
-            }
-        }
-        return output;
-    } catch (err) {
-        throw err;
+        /* Push the ICourse object to the output array */
+        output.push(course);
+      }
     }
+    return output;
+  } catch (err) {
+    throw err;
+  }
 }
 
 export { prepareSheet, getJSON };
