c78e561ec2fe80c5e9eb59973a8ad9aede69afe1
AloofSkip <nic.veloce@gmail.com>
2022-12-24 11:16:13 -0800

    use empty function in inngest function; inngest won't stop calling it

--- pages/api/inngest.ts
+++ pages/api/inngest.ts
@@ -5,7 +5,7 @@ import { serve } from "inngest/next";
 const scrape = createScheduledFunction(
   "Scrape Google Sheets", // The name of your function, used for observability.
   "16 * * * *", // The cron syntax for the function
-  updateDB // The function code, defined above.
+  () => {} // The function code, defined above.
 );
 
 export default serve("Scrape Google Sheets", [scrape]);
