63a603efe2dbbc264f07318f96190c17d4a6a95e
AloofSkip <nic.veloce@gmail.com>
2023-05-06 18:32:28 -0700

    revert changes to scraper script in hopes of resolving errors

--- scraper/src/scheduler/updateDB.ts
+++ scraper/src/scheduler/updateDB.ts
@@ -1,25 +1,17 @@
-import { convertRowsToJSON, copyRowsToMasterSheet, getMasterRows, getNewSurveyResponsesAsRows } from "./helper/sheets";
-import { updateCourseSummary, upsertNewCourses } from "./helper/summarize";
+import { prepareSheet, getJSON } from "./helper/sheets";
+import { saveCourses } from "./helper/summarize";
 import { connectToDatabase, disconnectFromDatabase } from "../mongodb";
 
 export default async function updateDB() {
   try {
     connectToDatabase();
-
-    /* Handle Invidual Course Data */
-    const newRows = await getNewSurveyResponsesAsRows();
-    const newRowJSON = convertRowsToJSON(newRows);
-    await upsertNewCourses(newRowJSON);
-    await copyRowsToMasterSheet(newRows);
-
-    /* Summarize Course Data */
-    const masterRows = await getMasterRows();
-    const masterDataJSON = convertRowsToJSON(masterRows);
-    await updateCourseSummary(masterDataJSON);
-
-    await disconnectFromDatabase();
+    await prepareSheet();
+    const json = await getJSON();
+    await saveCourses(json);
   } catch (err) {
     console.error(err);
+  } finally {
+    await disconnectFromDatabase();
   }
 }
 
